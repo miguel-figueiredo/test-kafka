5c8bd93e2506a55cb7809906a971ae72bcbbe8ab
Miguel Figueiredo <miguel.pedro@talkdesk.com>
2020-03-20 12:35:11 +0000

    Add producers and consumers container to compose.

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DOCKER_HOST_IP=$(ifconfig | grep "inet " | grep -v 127.0.0.1 | cut -d\  -f2)
+mvn package -T C1 && docker-compose build && docker-compose up -d zoo1 kafka1 kafdrop
+
+until (echo dump | nc localhost 2181 | grep brokers); do sleep 1; done
+kafka-topics --create --topic transcriptions -zookeeper localhost:2181 --replication-factor 1 --partitions 4
+kafka-topics --create --topic transcriptions-state -zookeeper localhost:2181 --replication-factor 1 --partitions 4
+
+docker-compose up kafka-producer kafka-consumer
--- test-kafka-dto/src/main/java/com/talkdesk/tdx/nlp/transcription/Transcription.java
+++ test-kafka-dto/src/main/java/com/talkdesk/tdx/nlp/transcription/Transcription.java
@@ -1,6 +1,5 @@
 package com.talkdesk.tdx.nlp.transcription;
 
-import io.vertx.core.json.JsonObject;
 import java.io.*;
 
 public class Transcription implements Serializable {
